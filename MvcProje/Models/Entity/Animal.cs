19011cb85621235a70317d7b5acc397d95e1fdad
selinnurgol <selinnur.gol@ogr.sakarya.edu.tr>
2022-08-13 14:44:47 +0300

    Animal class create

---
+++ MvcProje/Models/Entity/Animal.cs
@@ -0,0 +1,13 @@
+﻿namespace MvcProje.Models.Entity
+{
+    public class Animal
+    {
+        public int AnimalID { get; set; }
+
+        public int AnimalAge { get; set; }
+
+        public string AnimalName { get; set; }
+
+        public string Description { get; set; }
+    }
+}
