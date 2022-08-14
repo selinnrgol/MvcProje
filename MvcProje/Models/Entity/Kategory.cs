a94ac88a38c150ea407b10f7ee840a56d47291e0
selinnurgol <selinnur.gol@ogr.sakarya.edu.tr>
2022-08-14 22:12:58 +0300

    use and category creat

---
+++ MvcProje/Models/Entity/Kategory.cs
@@ -0,0 +1,14 @@
+﻿namespace MvcProje.Models.Entity
+{
+    public class Kategory
+    {
+
+        public int CategoryID { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public bool CategoryStatus { get; set; }
+
+
+    }
+}
---
+++ MvcProje/Models/Entity/User.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcProje.Models.Entity
+{
+    public class User
+    {
+        public int UserID { get; set; }
+
+        public string UserName { get; set; }
+
+        public string UserSurname { get; set; }
+
+    }
